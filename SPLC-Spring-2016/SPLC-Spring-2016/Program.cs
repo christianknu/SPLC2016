8c70433f846faf83fcf7708a5bcda34ef8719326
Christian Knudsen <cknu@itu.dk>
2016-01-30 19:33:52 +0100

    My first Hello World C# program - true, but not my first C# program ;-)

---
+++ SPLC-Spring-2016/SPLC-Spring-2016/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPLC_Spring_2016
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!\nTo exit this application just press Enter");
+            Console.WriteLine("Please enter your name:");
+            var userInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(userInput)) return;
+
+            while (true)
+            {
+                Console.WriteLine("\nHello " + userInput);
+                Console.WriteLine("Try to enter another name:");
+                userInput = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(userInput)){ continue; }
+
+                break;
+            }
+        }
+    }
+}
