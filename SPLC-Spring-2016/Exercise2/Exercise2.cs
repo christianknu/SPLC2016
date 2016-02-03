7e975d46bbefa371a37f830515a58ade752dc238
Christian Knudsen <cknu@itu.dk>
2016-02-03 16:13:24 +0100

    Exercise 2 & 3

---
+++ SPLC-Spring-2016/Exercise2/Exercise2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    public interface IExpression
+    {
+        double Eval();
+    }
+
+    public abstract class CompositeExpression : IExpression
+    {
+        public IExpression leftArgument, rightArgument;
+
+        public CompositeExpression(IExpression iExLeft, IExpression iExRight)
+        {
+            this.leftArgument = iExLeft;
+            this.rightArgument = iExRight;
+        }
+
+        public abstract double Eval();
+    }
+
+    public class Constant : IExpression
+    {
+        private double value;
+
+        public Constant(double val)
+        {
+            this.value = val;
+        }
+
+        public double Eval()
+        {
+            return this.value;
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+    }
+
+    public class Sum : CompositeExpression
+    {
+        public Sum(IExpression iExLeft, IExpression iExRight) : base(iExLeft, iExRight) { }
+
+        public override double Eval()
+        {
+            return leftArgument.Eval() + rightArgument.Eval();
+        }
+
+        public override string ToString()
+        {
+            return "(" + leftArgument.ToString() + " + " + rightArgument.ToString() + ")";
+        }
+    }
+
+    public class Product : CompositeExpression
+    {
+        public Product(IExpression iExLeft, IExpression iExRight) : base(iExLeft, iExRight) { }
+
+        public override double Eval()
+        {
+            return leftArgument.Eval() * rightArgument.Eval();
+        }
+
+        public override string ToString()
+        {
+            return leftArgument.ToString() + " * " + rightArgument.ToString();
+        }
+    }
+
+    class TextExpression
+    {
+        public static void Main()
+        {
+            IExpression pi = new Constant(3.4);
+            Console.WriteLine("Pi = {0}", pi.Eval());
+
+
+            // Representing the tree on the left: 2*(3+4)
+            IExpression threePlusFour = new Sum(new Constant(3), new Constant(4));
+            Console.WriteLine("threePlusFour = {0}", threePlusFour.Eval());
+
+            IExpression leftTree = new Product(new Constant(2), threePlusFour);
+            Console.WriteLine("leftTree: {1} = {0}", leftTree.Eval(), leftTree.ToString());
+
+            // Representing the tree on the right: 2*3+4
+            IExpression twoTimesThree = new Product(new Constant(2), new Constant(3));
+            Console.WriteLine("twoTimesThree = {0}", twoTimesThree.Eval());
+
+            IExpression rightTree = new Sum(twoTimesThree, new Constant(4));
+            Console.WriteLine("rightTree: {1} = {0}", rightTree.Eval(), rightTree.ToString());
+        }
+    }
+}
---
+++ SPLC-Spring-2016/Exercise3/TestMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class B
+    {
+        public static void SM() { Console.WriteLine("Hello from B.SM()"); }
+        public virtual void VIM() { Console.WriteLine("Hello from B.VIM()"); }
+        public void NIM() { Console.WriteLine("Hello from B.NIM()"); }
+    }
+
+    class C : B
+    {
+        public static new void SM() { Console.WriteLine("Hello from C.SM()"); }
+
+        public override void VIM() { Console.WriteLine("Hello from C.VIM()"); }
+
+        public new void NIM() { Console.WriteLine("Hello from C.NIM()"); }
+    }
+
+    class LetsRunIt
+    {
+        public static void Main()
+        {
+            B.SM();
+            C.SM();
+            B b = new C();
+            C c = new C();
+            b.VIM();
+            b.NIM();
+            c.VIM();
+            c.NIM();
+        }
+    }
+}
