0cf9fe558cbca90fc2987fab8b8a465fe12dfc22
Christian Knudsen <cknu@itu.dk>
2016-02-03 17:42:31 +0100

    Exercise 3 update.

--- SPLC-Spring-2016/Exercise3/TestMethods.cs
+++ SPLC-Spring-2016/Exercise3/TestMethods.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise3
 {
@@ -26,14 +22,34 @@ namespace Exercise3
     {
         public static void Main()
         {
+            Console.Write("\n_____________________________________\n\n");
+
+            Console.WriteLine("B.SM() - Calls the static SM() method in the B class:");
             B.SM();
+            Console.Write("_____________________________________\n");
+
+            Console.WriteLine("\nC.SM() - Calls the static SM() method in the C class:");
             C.SM();
+            Console.Write("_____________________________________\n");
+
             B b = new C();
-            C c = new C();
+            var c = new C();
+
+            Console.WriteLine("\nb.VIM() - Calls the VIM() method in the C class, because the run-time type of the instance (which is C), not the compile-time type of the instance (which is B), determines the actual method implementation to invoke:");
             b.VIM();
+            Console.Write("_____________________________________\n");
+
+            Console.WriteLine("\nb.NIM() - Calls the NIM()-method in the B class, as b is of compile-time type B, and the compile-time type is used when the method is non-virtual:");
             b.NIM();
+            Console.Write("_____________________________________\n");
+
+            Console.WriteLine("\nc.VIM() - Calls the VIM()-method in the C class, as c is of compile-time type C, and the compile-time type is used when the method is virtual:");
             c.VIM();
+            Console.Write("_____________________________________\n");
+
+            Console.WriteLine("\nc.NIM() - Calls the NIM()-method in the C class, as c is of run-time type C, and the run-time type is used when the method is non-virtual:");
             c.NIM();
+            Console.Write("_____________________________________\n");
         }
     }
 }
